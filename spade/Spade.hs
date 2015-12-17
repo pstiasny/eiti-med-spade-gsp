b166d8e03f1e7aa32384efab42e45a42957bc7eb
Paweł Stiasny <pawelstiasny@gmail.com>
2015-12-17 02:04:01 +0100

    sprzątanie

--- spade/Spade.hs
+++ spade/Spade.hs
@@ -17,58 +17,54 @@ data Sequence = Sequence [Atom]
 instance Show Sequence where
     show (Sequence []) = "{}"
     show (Sequence atoms) = foldl (++) "" (map show $ reverse atoms)
-seqLength :: Sequence -> Int
-seqLength (Sequence l) = length l
 
 data Node = Node { seq_ :: Sequence,
                    idList :: IdList,
                    support :: Int,
                    generatedBy :: [Node] } deriving (Show)
 
+
 loadDatabase :: Database -> [Node]
-loadDatabase db = map makeAtom db
-    where makeAtom (item, idList) = Node {seq_ = Sequence [SequenceAtom item],
-                                          idList = idList,
-                                          support = supportOf idList,
-                                          generatedBy = []}
+loadDatabase db = 
+    let makeAtom (item, idList) = Node {seq_ = Sequence [SequenceAtom item],
+                                        idList = idList,
+                                        support = supportOf idList,
+                                        generatedBy = []}
+    in  map makeAtom db
 
 
 generateCandidates :: Node -> Node -> [Node]
 generateCandidates left right =
     assert (prefixLeft == prefixRight) $
-    case (atomLeft, atomRight) of
-        (EventAtom al, EventAtom ar) ->
-            if al <= ar then [] else [
-                makeNode atomRight atomLeft (eventMatch idsLeft idsRight)
-            ]
-        (EventAtom al, SequenceAtom ar) -> [
-                makeNode atomLeft atomRight (temporalMatch idsLeft idsRight)
-            ]
-        (SequenceAtom al, EventAtom ar) ->
-            case (compare al ar) of
-                LT -> [makeNode atomLeft atomRight (eventMatch idsLeft idsRight)]
+
+    case (al, ar) of
+        (EventAtom il, EventAtom ir) ->
+            if il <= ir
+                then []
+                else [makeNode ar al eventMatched]
+
+        (EventAtom il, SequenceAtom ir) -> [makeNode al ar temporalMatched]
+
+        (SequenceAtom il, EventAtom ir) ->
+            case compare il ir of
+                LT -> [makeNode al ar eventMatched]
                 EQ -> []
-                GT -> [makeNode (SequenceAtom ar)
-                                (EventAtom al)
-                                (eventMatch idsLeft idsRight)]
-        (SequenceAtom al, SequenceAtom ar) ->
-            case (compare al ar) of
-                LT -> [
-                    makeNode atomLeft atomRight (temporalMatch idsLeft idsRight),
-                    makeNode atomLeft (EventAtom ar) (eventMatch idsLeft idsRight)
-                    ]
-                EQ -> [makeNode atomRight atomLeft (temporalMatch idsLeft idsRight)]
-                GT -> [
-                    makeNode atomLeft atomRight (temporalMatch idsLeft idsRight)
-                    ]
-    where Sequence (atomLeft:prefixLeft) = seq_ left
-          Sequence (atomRight:prefixRight) = seq_ right
-          idsLeft = idList left
-          idsRight = idList right
+                GT -> [makeNode (SequenceAtom ir) (EventAtom il) eventMatched]
+
+        (SequenceAtom il, SequenceAtom ir) ->
+            case compare il ir of
+                LT -> [makeNode al ar temporalMatched,
+                       makeNode al (EventAtom ir) eventMatched]
+                EQ -> [makeNode al ar temporalMatched]
+                GT -> [makeNode al ar temporalMatched]
+
+    where Node {seq_ = Sequence (al:prefixLeft), idList = idsLeft} = left
+          Node {seq_ = Sequence (ar:prefixRight), idList = idsRight} = right
+          eventMatched = eventMatch idsLeft idsRight
+          temporalMatched = temporalMatch idsLeft idsRight
           makeNode a1 a2 idList = Node {seq_ = Sequence (a2:a1:prefixLeft),
                                         idList = idList,
                                         support = supportOf idList,
-                                        {-generatedBy = [left, right]}-}
                                         generatedBy = [left]}
 
 
@@ -84,6 +80,7 @@ enumerateFrequentSeq minSup nodes =
         generateClass leftAtom =
             foldMap (temporalJoin minSup leftAtom) nodes
 
+        prefixClasses :: [[Node]]
         prefixClasses = map generateClass nodes
 
     in (foldMap (enumerateFrequentSeq minSup) prefixClasses) ++ nodes
