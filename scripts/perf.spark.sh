091ae4c6fbca8ca38698d7105cad2543f739c41d
Paweł Stiasny <pawelstiasny@gmail.com>
2016-01-25 15:10:26 +0100

    zwiększ parametry

--- scripts/perf.spark.sh
+++ scripts/perf.spark.sh
@@ -4,15 +4,15 @@
 #rm -f GSP_LOG SPADE_LOG
 
 for cores in 1 2 3 4; do
-    scripts/run_spark_experiment 10000 100 3 10 0.4 ${cores}
+    scripts/run_spark_experiment 200000 100 3 10 0.4 ${cores}
 done
 
-for num_sequences in 10 100 1000 10000 100000 1000000; do
+for num_sequences in 10 100 1000 10000 100000 500000 1000000; do
     scripts/run_spark_experiment ${num_sequences} 100 3 10 0.4 4
 done
 
-for support in $(seq 1 2 9); do
-    scripts/run_spark_experiment 10000 100 3 10 0.${support} 4
+for support in $(seq 1 9); do
+    scripts/run_spark_experiment 200000 100 3 10 0.${support} 4
 done
 #22*4*4*3*2
 
